8224f2e828a91c4020082faae35b5e8f2f403fba
Phirocbnits <phiroze.chowdhury@cbnits.com>
2022-06-02 09:48:13 +0530

    commit python files

---
+++ switcher.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    if (y):
+        return x/y
+    else:
+        return "invalid data"
+def default(x,y):
+    return "wrong option"
+cased = {1:add,2:sub,3:mul,4:div}
+print("1:add \n2:sub\n3:mul\n4:div")
+c=int(input("enter your choice:"))
+x,y=map(int,input("enter 2 num:").split())
+print(cased.get(c,default)(x,y))
+
