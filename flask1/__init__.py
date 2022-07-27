8c450dcc1ff27006aec3131a2f1d3b3078f50492
phirocbnits <106677038+phirocbnits@users.noreply.github.com>
2022-07-27 11:52:38 +0530

    Update __init__.py
    env added

--- flask1/__init__.py
+++ flask1/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root:root@localhost/flask1'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://{}:{}@{}:{}/{}'.format(environ.get('USER_NAME'),environ.get('MYSQL_ROOT_PASSWORD'),environ.get('HOST'),,environ.get('PORT')environ.get('DB'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 app.config['SECRET_KEY'] = '9b3eeae24ff5b6a5ca11c479'
 db = SQLAlchemy(app)
